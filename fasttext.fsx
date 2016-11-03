c0355e57fd57052b15fe19b700a3d8e49aa55318
Shelin Anton <shelinanton@gmail.com>
2016-11-04 00:06:07 +0300

    fixed paths to data files

--- fasttext.fsx
+++ fasttext.fsx
@@ -8,7 +8,7 @@ let trainData = Input.FilePath("./data/dbpedia.train")
 //we train our classifier in 4 threads, using 2 wordgrams and default model args, verbosive, without pretrained vectors and with label prefix "__label__"
 let state = Classifier.train(trainData, 4, Classifier.args, 2uy, "__label__", true, None)
 
-let testData = Input.FilePath("D:/ft/data/dbpedia.test")
+let testData = Input.FilePath("./data/dbpedia.test")
 let r = Classifier.test(state, 1, FileReader.streamToLines testData)
 printfn "%A" r
 assert(r.precision >= 9.8f) 
@@ -20,10 +20,10 @@ let prf =  pr |> Seq.head
               |> fst
 assert(prf = "__label__9")
 
-let vecTrainData = Input.FilePath("D:/ft/data/text9")
+let vecTrainData = Input.FilePath("./data/text9")
 let skipgram = Vectorizer.train(vecTrainData,4,Vectorizer.args,Args.VecModel.sg, 3uy, 6uy, true)
 
-let words = Input.FilePath("D:/ft/data/queries.txt") |> FileReader.streamToWords
+let words = Input.FilePath("./data/queries.txt") |> FileReader.streamToWords
 let wrodsWithVectors = Vectorizer.getWordVectors(skipgram,words)
 
 // FastTextM.saveState "path" state
